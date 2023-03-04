2c96807a363128d45a6cd80be601ef8d04ec08b5
Farid <faridguluzade26@gmail.com>
2023-03-03 19:02:29 -0800

    adding redux-persist

--- src/router/navigation/navigation.component.jsx
+++ src/router/navigation/navigation.component.jsx
@@ -6,7 +6,6 @@ import { selectIsCartOpen } from "../../store/cart/cart.selector";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
-import { CartContext } from "../../contexts/cart-context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import { selectCurrentUser } from "../../store/user/user.selector";
 import {
