25d9ada22557033da61a2e96645a51986cdf45e4
Farid <faridguluzade26@gmail.com>
2023-03-07 12:45:01 -0800

    updating sign out saga and sign in saga

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
+  getCurrentUser,
 } from "./utils/firebase/firebase.utils";
 import Home from "./router/home/home.component";
 import Navigation from "./router/navigation/navigation.component";
@@ -11,18 +12,13 @@ import Authentication from "./router/authentication/authentication.component";
 import Shop from "./router/shop/shop.component";
 import Checkout from "./router/checkout/checkout.component";
 import { setCurrentUser } from "./store/user/user.action";
+import { checkUserSession } from "./store/user/user.action";
 
 const App = () => {
   const dispacth = useDispatch();
 
   useEffect(() => {
-    const unsubscripe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-      dispacth(setCurrentUser(user));
-    });
-    return unsubscripe;
+    dispacth(checkUserSession());
   }, []);
 
   return (
